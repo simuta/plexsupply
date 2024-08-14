6a1d2114ae945d19b731ac3bdd11b3d2c47428d1
nsimu <simuta@i.ua>
2024-08-14 15:50:06 +0300

    origin commit

---
+++ src/test/java/pages/MainPage.java
@@ -0,0 +1,8 @@
+package pages;
+
+import org.openqa.selenium.By;
+
+public class MainPage {
+    public static By searchFieldBy = By.id("search");
+    public static By searchButtondBy = By.xpath("//*[@id=\"search_mini_form\"]/div[2]/button");
+}
